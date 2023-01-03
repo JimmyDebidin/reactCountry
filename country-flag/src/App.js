cc4d0f1ab09ce4fff1821fa1ec63d0aa9ef202c4
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2023-01-03 13:34:06 +0100

    delete file package.json at root

--- country-flag/src/App.js
+++ country-flag/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-	return <div class="color-bg">helo</div>;
+	return (
+		<div className="helloWord">
+			<h1>il etait unbe fois</h1>
+		</div>
+	);
 };
 
 export default App;
